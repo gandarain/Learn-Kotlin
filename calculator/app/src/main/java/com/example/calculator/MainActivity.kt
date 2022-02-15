bf063960cc1431702953accb54216ce1517fa5de
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-02-15 21:44:40 +0700

    add functionlity clear and digit

--- calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,15 +3,25 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var tvInput: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
     }
 
     fun onDigit(view: View) {
-        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
+        tvInput?.append((view as Button).text)
+    }
+
+    fun onClear(view: View) {
+        tvInput?.text = ""
     }
 }
