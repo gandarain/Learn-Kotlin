3e815441bb7a3b608e1e5da43e5f5c65ffc5d546
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-02-15 21:02:27 +0700

    setup calculator app

---
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View) {
+        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
+    }
+}
