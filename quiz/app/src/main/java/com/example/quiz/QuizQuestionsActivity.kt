b43b77df66d72fed9f3adccc057a19365e07ad38
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-02-16 22:52:21 +0700

    create model for questions list

--- quiz/app/src/main/java/com/example/quiz/QuizQuestionsActivity.kt
+++ quiz/app/src/main/java/com/example/quiz/QuizQuestionsActivity.kt
@@ -2,10 +2,14 @@ package com.example.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        var questionsList: ArrayList<Question> = Constants.getQuestions()
+        Log.i("Question list size is", "${questionsList.size}")
     }
 }
