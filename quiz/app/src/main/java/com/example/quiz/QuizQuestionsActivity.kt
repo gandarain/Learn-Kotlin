ea5cf2c4635166815e6f4772cc8f85f65bdf24d6
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-02-17 22:12:02 +0700

    implement selected option text view

--- quiz/app/src/main/java/com/example/quiz/QuizQuestionsActivity.kt
+++ quiz/app/src/main/java/com/example/quiz/QuizQuestionsActivity.kt
@@ -1,13 +1,16 @@
 package com.example.quiz
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private var textViewQuestion: TextView? = null
     private var imageViewFlag: ImageView? = null
     private var progressBar: ProgressBar? = null
@@ -19,13 +22,14 @@ class QuizQuestionsActivity : AppCompatActivity() {
     private var buttonSubmit: Button? = null
 
     private var currentProgress: Int = 1
+    private var questionsList: ArrayList<Question>? = null
+    private var selectedOption: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-        var questionsList: ArrayList<Question> = Constants.getQuestions()
-        var question: Question = questionsList[currentProgress-1]
+        questionsList = Constants.getQuestions()
 
         textViewQuestion = findViewById(R.id.textViewQuestion)
         imageViewFlag = findViewById(R.id.imageViewFlag)
@@ -37,13 +41,76 @@ class QuizQuestionsActivity : AppCompatActivity() {
         optionFour = findViewById(R.id.optionFour)
         buttonSubmit = findViewById(R.id.buttonSubmit)
 
+        optionOne?.setOnClickListener(this)
+        optionTwo?.setOnClickListener(this)
+        optionThree?.setOnClickListener(this)
+        optionFour?.setOnClickListener(this)
+        buttonSubmit?.setOnClickListener(this)
+
+        setQuestions()
+    }
+
+    private fun setQuestions() {
+        var question: Question = questionsList!![currentProgress-1]
         textViewQuestion?.text = question.question
         imageViewFlag?.setImageResource(question.image)
         progressBar?.progress = currentProgress
-        textViewProgress?.text = "${currentProgress}/${questionsList.size}"
+        textViewProgress?.text = "${currentProgress}/${progressBar?.max}"
         optionOne?.text = question.optionFour
         optionTwo?.text = question.optionTwo
         optionThree?.text = question.optionThree
         optionFour?.text = question.optionFour
+
+        if (currentProgress == questionsList!!.size) {
+            buttonSubmit?.text = "FINISH"
+        } else {
+            buttonSubmit?.text = "SUBMIT"
+        }
+    }
+
+    private fun setDefaultOptionView() {
+        val options: ArrayList<TextView> = ArrayList<TextView>()
+
+        optionOne?.let { options.add(0, it) }
+        optionTwo?.let { options.add(1, it) }
+        optionThree?.let { options.add(2, it) }
+        optionFour?.let { options.add(3, it) }
+
+        for (option in options) {
+            option.setTextColor(ContextCompat.getColor(this, R.color.secondaryTextColor))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
+        }
+    }
+
+    private fun onSelectOption(textViewSelected: TextView, selectedOption: Int) {
+        // set default all the option into default
+        setDefaultOptionView()
+        this.selectedOption = selectedOption
+
+        // set the selected option with styling
+        textViewSelected.setTextColor(ContextCompat.getColor(this, R.color.purple_500))
+        textViewSelected.setTypeface(textViewSelected.typeface, Typeface.BOLD)
+        textViewSelected.background = ContextCompat.getDrawable(this, R.drawable.selected_option_border_bg)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            R.id.optionOne -> {
+                optionOne?.let { onSelectOption(it, 1) }
+            }
+            R.id.optionTwo -> {
+                optionTwo?.let { onSelectOption(it, 2) }
+            }
+            R.id.optionThree -> {
+                optionThree?.let { onSelectOption(it, 3) }
+            }
+            R.id.optionFour -> {
+                optionFour?.let { onSelectOption(it, 4) }
+            }
+            R.id.buttonSubmit -> {
+                // TODO "Implement button submit"
+            }
+        }
     }
 }
