c81b53868f4ce7331175e6b5fd00be5325ace95b
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-02-17 21:33:30 +0700

    setup ui options

--- quiz/app/src/main/java/com/example/quiz/QuizQuestionsActivity.kt
+++ quiz/app/src/main/java/com/example/quiz/QuizQuestionsActivity.kt
@@ -2,14 +2,48 @@ package com.example.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionsActivity : AppCompatActivity() {
+    private var textViewQuestion: TextView? = null
+    private var imageViewFlag: ImageView? = null
+    private var progressBar: ProgressBar? = null
+    private var textViewProgress: TextView? = null
+    private var optionOne: TextView? = null
+    private var optionTwo: TextView? = null
+    private var optionThree: TextView? = null
+    private var optionFour: TextView? = null
+    private var buttonSubmit: Button? = null
+
+    private var currentProgress: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
         var questionsList: ArrayList<Question> = Constants.getQuestions()
-        Log.i("Question list size is", "${questionsList.size}")
+        var question: Question = questionsList[currentProgress-1]
+
+        textViewQuestion = findViewById(R.id.textViewQuestion)
+        imageViewFlag = findViewById(R.id.imageViewFlag)
+        progressBar = findViewById(R.id.progressBar)
+        textViewProgress = findViewById(R.id.textViewProgress)
+        optionOne = findViewById(R.id.optionOne)
+        optionTwo = findViewById(R.id.optionTwo)
+        optionThree = findViewById(R.id.optionThree)
+        optionFour = findViewById(R.id.optionFour)
+        buttonSubmit = findViewById(R.id.buttonSubmit)
+
+        textViewQuestion?.text = question.question
+        imageViewFlag?.setImageResource(question.image)
+        progressBar?.progress = currentProgress
+        textViewProgress?.text = "${currentProgress}/${questionsList.size}"
+        optionOne?.text = question.optionFour
+        optionTwo?.text = question.optionTwo
+        optionThree?.text = question.optionThree
+        optionFour?.text = question.optionFour
     }
 }
