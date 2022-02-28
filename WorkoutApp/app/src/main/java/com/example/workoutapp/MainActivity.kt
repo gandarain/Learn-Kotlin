13d1601ca9e78eb7ff31577a983b002acd809a88
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-02-28 10:59:51 +0700

    create the start UI screen

--- WorkoutApp/app/src/main/java/com/example/workoutapp/MainActivity.kt
+++ WorkoutApp/app/src/main/java/com/example/workoutapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonStart: FrameLayout = findViewById(R.id.buttonStart)
+        buttonStart.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Here will be start the exercise",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
