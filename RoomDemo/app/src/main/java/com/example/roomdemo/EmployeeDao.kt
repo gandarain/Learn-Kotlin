28991b8ce5b19d0c91d6ce64490179a04422679e
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-03-05 10:33:27 +0700

    ### Create Dao & Database - Create DAO (Database Access Object) to access the data - Create Database

---
+++ RoomDemo/app/src/main/java/com/example/roomdemo/EmployeeDao.kt
@@ -0,0 +1,30 @@
+package com.example.roomdemo
+
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+// Todo 1: create the dao interface
+@Dao
+interface EmployeeDao {
+    // Todo 2: create a suspend insert function for saving an entry
+    @Insert
+    suspend fun insert(employeeEntity: EmployeeEntity)
+
+    // Todo 3: create a suspend update function for updating an existing entry
+    @Update
+    suspend fun update(employeeEntity: EmployeeEntity)
+
+    // Todo 4: create a suspend delete function for deleting an existing entry
+    @Delete
+    suspend fun delete(employeeEntity: EmployeeEntity)
+
+    // Todo 5: create a function to read all employee, this returns a Flow
+    @Query("SELECT * FROM `employee-table`")
+    // flow is used to hold a value that can change at runtime
+    fun fetchAllEmployees(): Flow<List<EmployeeEntity>>
+
+    // Todo 6: create a function to read one employee, this returns a Flow
+    @Query("SELECT * FROM `employee-table` WHERE id=:id")
+    fun fetchAllEmployeeById(id: Int): Flow<List<EmployeeEntity>>
+}
