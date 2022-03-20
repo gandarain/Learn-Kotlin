2585380d87b461f354e18529fc578a7d2ae55a27
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-03-20 19:43:20 +0700

    ### Preparing Permission Requests Using Dexter
    - Create Dexter listener.
    - Get the Lat & Lng when all the permissions are enabled.
    - Show toast if all of the permissions are disabled.
    - Show rational dialog if permission is able to request again.
    - Create a new lateinit var mFusedLocationClient and store the current location into that variable.

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,17 +1,35 @@
 package com.example.weatherapp
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.location.LocationManager
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
+import android.os.Looper
 import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.*
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         checkPermission()
     }
@@ -20,17 +38,9 @@ class MainActivity : AppCompatActivity() {
     // START
     private fun checkPermission(): Unit {
         if (isLocationEnabled()) {
-            Toast.makeText(
-                this,
-                "Your location provider is already ON.",
-                Toast.LENGTH_SHORT
-            ).show()
+            checkPermissionLocationDexter()
         } else {
-            Toast.makeText(
-                this,
-                "Your location provider is turned off. Please turn it on.",
-                Toast.LENGTH_SHORT
-            ).show()
+            showToastText("Your location provider is turned off. Please turn it on.")
 
             // This will redirect you to settings from where you need to turn on the location provider.
             val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
@@ -38,10 +48,111 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // TODO (Asking the location permission on runtime.)
+    // START
+    private fun checkPermissionLocationDexter() {
+        Dexter.withActivity(this).withPermissions(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ).withListener(
+            object: MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report!!.areAllPermissionsGranted()) {
+                        requestLocationData()
+                    }
+
+                    if (report.isAnyPermissionPermanentlyDenied) {
+                        showToastText("You have denied location permission.")
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    // TODO show dialog rationale dialog for permission
+                    showRationaleDialogPermissions()
+                }
+            }
+        ).onSameThread().check()
+    }
+
+    // TODO (A alert dialog for denied permissions and if needed to allow it from the settings app info.)
+    // START
+    /**
+     * A function used to show the alert dialog when the permissions are denied and need to allow it from settings app info.
+     */
+    private fun showRationaleDialogPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It looks like you have turned off permissions")
+                // _ => parameter is not used
+            .setPositiveButton("GO TO SETTINGS") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+    /**
+     * A function which is used to verify that the location or GPS is enable or not of the user's device.
+     */
     private fun isLocationEnabled(): Boolean {
         // This provide access to system location services.
         var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
             LocationManager.NETWORK_PROVIDER)
+    }
+
+    // TODO (Add a function to get the location of the device using the fusedLocationProviderClient.)
+    // START
+    /**
+     * A function to request the current location. Using the fused location provider client.
+     */
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData() {
+
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        Looper.myLooper()?.let {
+            mFusedLocationClient.requestLocationUpdates(
+                mLocationRequest, mLocationCallback,
+                it
+            )
+        }
+    }
+
+    // TODO (Register a request location callback to get the location.)
+    // START
+    /**
+     * A location callback object of fused location provider client where we will get the current location details.
+     */
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location = locationResult.lastLocation
+            val latitude = mLastLocation.latitude
+            Log.i("Current Latitude", "$latitude")
+
+            val longitude = mLastLocation.longitude
+            Log.i("Current Longitude", "$longitude")
+        }
+    }
+
+    private fun showToastText(
+        text: String
+    ) {
+        Toast.makeText(
+            this@MainActivity,
+            text,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
